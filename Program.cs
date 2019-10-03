70c072bf9dae206bfc5dae9dec8329a6d9294670
David Tonoyan <davtonoyan1998@mail.ru>
2019-10-03 23:08:03 +0400

    Shataxos

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Beispiel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                     
+           Mard x = new Mard();
+           
+           Console.WriteLine("You are talkative true or false?");           
+                    
+           x.Shataxosutyun = Convert.ToBoolean(Console.ReadLine());
+           Console.WriteLine(x.Shataxosutyun);
+        }
+
+    }
+    class Mard 
+
+    {
+        public bool Shataxosutyun
+
+        {
+            set
+            {
+                if (value == true)
+                { 
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.Beep();
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < 2; i++)
+                    Console.Beep();
+                                   }
+                 Console.WriteLine("You speak little");
+            }
+            get
+            {
+                return Shataxosutyun; 
+            }
+
+        }
+
+    }
+}
